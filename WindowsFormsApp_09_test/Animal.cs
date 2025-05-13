1debd4b2d17856efe520f0fee72f8a37597cf164
jjy0307 <adz7546@gmail.com>
2025-05-13 11:35:45 +0900

    초기 커밋.

---
+++ WindowsFormsApp_09_test/Animal.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp_09_test
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+
+        public void Speak()
+        {
+            Console.WriteLine($"{Name}: 동물이 소리를 냅니다.");
+        }
+
+    }
+    public class Dog : Animal
+    {
+        public Dog() : base() { }
+
+        public void DogSpeak()
+        {
+            Console.WriteLine("멍");
+        }
+
+    }
+
+    public class Cat : Animal
+    {
+        public Cat() : base() { }
+
+        public void CatSpeak()
+        {
+            Console.WriteLine("야옹");
+        }
+
+    }
+
+    public class Bird : Animal
+    {
+        public Bird() : base() { }
+
+        public void BirdSpeak()
+        {
+            Console.WriteLine("짹");
+        }
+
+    }
+}
---
+++ WindowsFormsApp_09_test/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_09_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // 업캐스팅
+            Animal dog = new Dog();
+            Animal bird = new Bird();
+            Animal cat = new Cat();
+
+            dog.Name = "강아지";
+            cat.Name = "고양이";
+            bird.Name = "새";
+
+            // 업캐스팅
+            dog.Speak();
+            cat.Speak();
+            bird.Speak();
+
+            //다운캐스팅
+            ((Dog)dog).DogSpeak();
+            ((Cat)cat).CatSpeak();
+            ((Bird)bird).BirdSpeak();
+        }
+    }
+}
